d51602a18da119c9f3d52c9d888242df7f9aec27
Rzavsky-ev <rzavsky.ev@gmail.com>
2025-05-06 20:10:21 +0300

    Первый коммит

---
+++ src/main/java/org/skypro/starbank/StarBankServiceApplication.java
@@ -0,0 +1,13 @@
+package org.skypro.starbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarBankServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StarBankServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/skypro/starbank/StarBankServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.skypro.starbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarBankServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
